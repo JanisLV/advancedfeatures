5834a5cd0e6e7c3481541909444b7f1483e843b9
denissfjodorovs <deniss.fjodorovs@mintos.com>
2020-06-14 15:55:29 +0300

    thread task

--- src/com/company/threads/employee/Employee.java
+++ src/com/company/threads/employee/Employee.java
@@ -5,9 +5,14 @@ import java.util.Date;
 
 public class Employee implements Runnable {
     private String name;
+    private volatile boolean flag = true;
 
     public Employee(String name) {
         this.name = name;
+    }
+
+    public void stopWorking(){
+        flag = false;
     }
 
     @Override
@@ -15,6 +20,14 @@ public class Employee implements Runnable {
         SimpleDateFormat format = new SimpleDateFormat("HH:mm");
         System.out.println(name +
                 " - I came to work at "
-                        + format.format(new Date()));
+                + format.format(new Date()));
+        while (flag) {
+            try {
+                Thread.sleep(10_000l);
+                System.out.println(name + " - I'm still working!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/com/company/threads/employee/Main.java
+++ src/com/company/threads/employee/Main.java
@@ -1,10 +1,28 @@
 package com.company.threads.employee;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
-        Employee john = new Employee("John");
-        Thread t1 = new Thread(john);
-        t1.start();
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Employee> employeeList = new ArrayList<>(List.of(
+                new Employee("John"),
+                new Employee("Janis"),
+                new Employee("Michal"),
+                new Employee("Dwight"),
+                new Employee("Andy")
+        ));
+
+        for (Employee employee : employeeList) {
+            Thread t = new Thread(employee);
+            t.start();
+        }
+
+        Thread.sleep(30_000l);
+
+        Employee employee = employeeList.get(0);
+        employee.stopWorking();
+        employeeList.remove(employee);
     }
 }
