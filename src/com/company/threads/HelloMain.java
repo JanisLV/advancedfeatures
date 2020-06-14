41964b0694b9555bdeece6d9f6432a0639e0c25f
denissfjodorovs <deniss.fjodorovs@mintos.com>
2020-06-14 15:09:54 +0300

    thread task

--- src/com/company/threads/HelloMain.java
+++ src/com/company/threads/HelloMain.java
@@ -1,9 +1,16 @@
 package com.company.threads;
 
 public class HelloMain {
-    public static void main(String[] args) {
-        HelloPrinter helloPrinter = new HelloPrinter();
-        Thread t = new Thread(helloPrinter);
-        t.start();
+    public static void main(String[] args) throws InterruptedException {
+        HelloPrinter helloPrinter1 = new HelloPrinter(1);
+        Thread t1 = new Thread(helloPrinter1);
+        t1.start();
+        helloPrinter1.stopPrinting();
+
+        HelloPrinter helloPrinter2 = new HelloPrinter(1);
+        Thread t2 = new Thread(helloPrinter2);
+        t2.start();
+        Thread.sleep(4000l);
+        helloPrinter2.stopPrinting();
     }
 }
--- src/com/company/threads/HelloPrinter.java
+++ src/com/company/threads/HelloPrinter.java
@@ -1,8 +1,32 @@
 package com.company.threads;
 
 public class HelloPrinter implements Runnable {
+    private int id;
+    private volatile boolean flag = true;
+
+    public HelloPrinter(int id) {
+        this.id = id;
+    }
+
+    public void stopPrinting(){
+        flag = false;
+    }
+
+    public void changeId(){
+        id++;
+    }
+
     @Override
     public void run() {
-        System.out.println("Hello!");
+        System.out.println("Hello!" + id);
+        while (flag){
+            changeId();
+            try {
+                Thread.sleep(1500l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("New value = " + id);
+        }
     }
 }
